6c0dd6b5ac03b3e430e7d1645bda6ba4bfda4f54
Caio Fernando <caiofsr13@gmail.com>
2024-05-27 11:06:32 -0300

    Add event dispatcher with struct, methods and tests

---
+++ .fcutils/pkg/events/event_dispatcher.go
@@ -0,0 +1,55 @@
+package events
+
+import "errors"
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ed.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			handler.Handle(event)
+		}
+	}
+
+	return nil
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+
+	return false
+}
